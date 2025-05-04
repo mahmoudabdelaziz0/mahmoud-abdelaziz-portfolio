491adda3289529b5a97b12ff356e54a90e32aec7
mahmoudabdelaziz0 <mahmoudcr106@gmail.com>
2025-05-04 23:31:50 +0300

    First commit with Firebase Hosting workflow

---
+++ lib/views/about_me.dart
@@ -0,0 +1,124 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/globals/app_assets.dart';
+import 'package:my_portfolio/globals/app_colors.dart';
+import 'package:my_portfolio/globals/constants.dart';
+import 'package:my_portfolio/helper%20class/helper_class.dart';
+import '../globals/app_buttons.dart';
+import '../globals/app_text_styles.dart';
+
+class AboutMe extends StatelessWidget {
+  const AboutMe({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return HelperClass(
+      mobile: Column( 
+        
+      
+        children: [
+          buildAboutMeContents(),
+          Constants.sizedBox(height: 35.0),
+          buildProfilePicture(),
+        ],
+      ),
+      tablet: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          buildProfilePicture(),
+          Constants.sizedBox(width: 25.0),
+          Expanded(child: buildAboutMeContents())
+        ],
+      ),
+      desktop: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          buildProfilePicture(),
+          Constants.sizedBox(width: 25.0),
+          Expanded(child: buildAboutMeContents())
+        ],
+      ),
+      paddingWidth: size.width * 0.1,
+      bgColor: AppColors.bgColor2,
+    );
+  }
+
+  FadeInRight buildProfilePicture() {
+    return FadeInRight(
+      duration: const Duration(milliseconds: 1200),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            height: 450,
+            width: 400,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: AppColors.robinEdgeBlue,
+                width: 2,
+              ),
+            ),
+            child: CircleAvatar(
+              child: Image.asset(
+                AppAssets.profile2,
+                height: 450,
+                width: 400,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+       
+        ],
+      ),
+    );
+  }
+
+  Column buildAboutMeContents() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        FadeInRight(
+          duration: const Duration(milliseconds: 1200),
+          child: RichText(
+            text: TextSpan(
+              text: 'About ',
+              style: AppTextStyles.headingStyles(fontSize: 30.0),
+              children: [
+                TextSpan(
+                  text: 'Me!',
+                  style: AppTextStyles.headingStyles(
+                      fontSize: 30, color: AppColors.robinEdgeBlue),
+                )
+              ],
+            ),
+          ),
+        ),
+        Constants.sizedBox(height: 6.0),
+        FadeInLeft(
+          duration: const Duration(milliseconds: 1400),
+          child: Text(
+            'Flutter Developer!',
+            style: AppTextStyles.montserratStyle(color: Colors.white),
+          ),
+        ),
+        Constants.sizedBox(height: 8.0),
+        FadeInLeft(
+          duration: const Duration(milliseconds: 1600),
+          child: Text(
+            'I\'m a Flutter Developer with hands-on experience in mobile app development and UI/UX implementation. Currently working as an IT Specialist, which has enhanced my understanding of real-world systems and backend infrastructure. I build clean, responsive apps using Flutter, with knowledge in Firebase, SQLite, APIs, and Git. (injected humour and the like).',
+            style: AppTextStyles.normalStyle(),
+          ),
+        ),
+        Constants.sizedBox(height: 15.0),
+        FadeInUp(
+          duration: const Duration(milliseconds: 1800),
+          child: AppButtons.buildMaterialButton(
+              onTap: () {}, buttonName: 'Read More'),
+        )
+      ],
+    );
+  }
+}
